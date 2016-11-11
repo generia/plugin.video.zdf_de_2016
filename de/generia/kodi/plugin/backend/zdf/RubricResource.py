bfd4a27ded62569d101318418052ca55d360a400
alex <alex@generia.de>
2016-11-11 03:05:45 +0100

    performance update rubric parsing

--- de/generia/kodi/plugin/backend/zdf/RubricResource.py
+++ de/generia/kodi/plugin/backend/zdf/RubricResource.py
@@ -8,11 +8,11 @@ from de.generia.kodi.plugin.backend.zdf.Regex import compile
 from de.generia.kodi.plugin.backend.zdf.Teaser import Teaser
 
 
-sectionListPattern = getTagPattern('[^ ]*', 'b-content-teaser-list')
+listPattern = compile('<[^ ]*>[^>]*class="([^"]*b-cluster m-filter[^"]*|[^"]*b-content-teaser-list[^"]*)"[^>]*>')
+
 sectionTitlePattern = compile('<h2 class="[^"]*title[^"]*"[^>]*>([^<]*)</h2>')
 sectionItemPattern = getTagPattern('article', 'b-content-teaser-item')
 
-clusterListPattern = getTagPattern('article', 'b-cluster m-filter')
 clusterTitlePattern = compile('<h2 class="[^"]*cluster-title[^"]*"[^>]*>([^<]*)</h2>')
 clusterItemPattern = getTagPattern('article', 'b-cluster-teaser')
 
@@ -46,7 +46,7 @@ class RubricResource(HtmlResource):
         self.clusters = []
         if self.listType is None:
             pos = 0
-            match = self._getNextMatch(self.content, pos)
+            match = listPattern.search(self.content, pos)
             while match is not None:
                 pos = match.end(0)
                 class_ = match.group(1)
@@ -63,7 +63,8 @@ class RubricResource(HtmlResource):
                     cluster = Cluster(title, listType, pos)
                     self.clusters.append(cluster)
     
-                match = self._getNextMatch(self.content, pos)
+                match = listPattern.search(self.content, pos)
+
                 if match is not None:
                     cluster.listEnd = match.start(0)-1
                 else:
@@ -84,113 +85,4 @@ class RubricResource(HtmlResource):
                     cluster.teasers.append(teaser)
 
                 itemMatch = itemPattern.search(self.content, pos)
-
-    def parse2(self):
-        super(RubricResource, self).parse()
-
-        self.clusters = []
-        pos = 0
-        match = self._getNextMatch(self.content, pos)
-        cluster = None
-        while match is not None:
-            pos = match.end(0)
-            class_ = match.group(1)
-            if self._isListMatch(class_):
-                titlePattern = clusterTitlePattern
-                if class_.find('b-content-teaser-list') != -1:
-                    titlePattern = sectionTitlePattern
-                titleMatch = titlePattern.search(self.content, pos)
-                if titleMatch is not None:
-                    title = stripHtml(titleMatch.group(1))
-                    pos = titleMatch.end(0)
-                    cluster = Cluster(title, [])
-                    self.clusters.append(cluster)
-            elif cluster is not None:
-                teaser = Teaser()
-                pos = teaser.parse(self.content, pos, match)
-                if teaser.valid():
-                    cluster.teasers.append(teaser)
-
-            match = self._getNextMatch(self.content, pos)
             
-    def _isListMatch(self, class_):
-        return class_.find('b-content-teaser-list') != -1 or class_.find('b-cluster m-filter') != -1 
-    
-    def _getNextMatch(self, string, pos=0):
-        sectionListMatch = sectionListPattern.search(string, pos)
-        clusterListMatch = clusterListPattern.search(string, pos)
-        if sectionListMatch is None and clusterListMatch is None:
-            return None
-        
-        if sectionListMatch is not None and clusterListMatch is None:
-            return sectionListMatch
-         
-        if sectionListMatch is None and clusterListMatch is not None:
-            return clusterListMatch
-        
-        if sectionListMatch.start(0) < clusterListMatch.start(0):
-            return sectionListMatch
-        
-        return clusterListMatch
-        '''        
-        sectionMatch = sectionPattern.search(self.content)
-        pos = 0
-        cluster = None
-        while sectionMatch is not None:
-            pos = sectionMatch.end(0)
-            class_ = sectionMatch.group(1)
-            if class_.find('b-content-teaser-item') == -1:
-                sectionTitleMatch = sectionTitlePattern.search(self.content, pos)
-                if sectionTitleMatch is not None:
-                    title = stripHtml(sectionTitleMatch.group(1))
-                    pos = sectionTitleMatch.end(0)
-                    cluster = Cluster(title, [])
-                    self.clusters.append(cluster)
-            elif cluster is not None:
-                teaser = Teaser()
-                pos = teaser.parse(self.content, pos, sectionMatch)
-                if teaser.valid():
-                    cluster.teasers.append(teaser)
-
-            sectionMatch = sectionPattern.search(self.content, pos)
-
-
-        # parse cluster teasers        
-        articleMatch = articlePattern.search(self.content, pos)
-        cluster = None
-        while articleMatch is not None:
-            pos = articleMatch.end(0)
-            class_ = articleMatch.group(1)
-            if class_.find('b-cluster-teaser') == -1:
-                clusterTitleMatch = clusterTitlePattern.search(self.content, pos)
-                if clusterTitleMatch is not None:
-                    title = stripHtml(clusterTitleMatch.group(1))
-                    pos = clusterTitleMatch.end(0)
-                    cluster = Cluster(title, [])
-                    self.clusters.append(cluster)
-            elif cluster is not None:
-                teaser = Teaser()
-                pos = teaser.parse(self.content, pos, articleMatch)
-                if teaser.valid():
-                    cluster.teasers.append(teaser)
-
-            articleMatch = articlePattern.search(self.content, pos)
-        '''
-            
-    def _parseClusterSection(self, clusterSection):
-
-        sectionTitleMatch = sectionTitlePattern.search(clusterSection)
-        if sectionTitleMatch is None:
-            return None
-        title = stripHtml(sectionTitleMatch.group(1))
-        pos = sectionTitleMatch.end(0)
-        
-        teasers = []
-        while pos != -1:
-            teaser = Teaser()
-            pos = teaser.parse(clusterSection, pos)
-            if teaser.valid():
-                teasers.append(teaser)
-            
-        #teasers = sorted(teasers, cmp=compareTeasers, reverse=True)
-        return Cluster(title, teasers)
